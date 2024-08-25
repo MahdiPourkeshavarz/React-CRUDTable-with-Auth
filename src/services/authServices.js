0e927bf825661e635165808b1af7d7c2aeadc941
mahdi keshavarz <69865082+ma-pk1996@users.noreply.github.com>
2024-08-25 15:39:27 +0330

    add : axios interceptors logic

---
+++ src/services/authServices.js
@@ -0,0 +1,54 @@
+/* eslint-disable no-unused-vars */
+import axios from "axios";
+import { API_URL } from "../constants";
+
+export const httpRequest = axios.create({
+  baseURL: API_URL.BASE_URL,
+});
+
+httpRequest.interceptors.request.use((req) => {
+  if (req.url !== API_URL.REFRESH_TOKEN_URL) {
+    const token = localStorage.getItem("refreshToken");
+    req.headers.refreshToken = token;
+  } else if (req.url !== API_URL.LOGIN_URL) {
+    const token = localStorage.getItem("accessToken");
+    req.headers.accessToken = token;
+  }
+  return req;
+});
+
+httpRequest.interceptors.response.use(
+  (res) => res,
+  async (error) => {
+    if (error.response.status !== 401) {
+      return Promise.reject(error);
+    }
+    const originalRequest = error.config;
+    if (
+      error.response.status === 403 &&
+      originalRequest.url === API_URL.REFRESH_TOKEN_URL
+    ) {
+      return Promise.reject(error);
+    }
+    if (originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        await getRefreshToken();
+        const res = httpRequest.post(originalRequest);
+        return Promise.resolve(res);
+      } catch (e) {
+        localStorage.removeItem("accessToken");
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
+async function getRefreshToken() {
+  try {
+    const response = await httpRequest.get(API_URL.REFRESH_TOKEN_URL);
+    return response.data;
+  } catch (e) {
+    return e.message;
+  }
+}

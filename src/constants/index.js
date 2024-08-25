9ffdcb8c108a97074c30319e1e148394356e28df
mahdi keshavarz <69865082+ma-pk1996@users.noreply.github.com>
2024-08-25 15:16:18 +0330

    add: adding api addresses

---
+++ src/constants/index.js
@@ -0,0 +1,17 @@
+const BASE_URL = "https://mohammadrezagh80.pythonanywhere.com";
+
+const REFRESH_TOKEN_URL = "/api/accounts/refresh/";
+
+const LOGIN_URL = "/api/accounts/login/";
+
+const REGISTER_URL = "/api/accounts/register/";
+
+const BASE_COURSE_URL = "/api/course-list/";
+
+export const API_URL = {
+  BASE_URL,
+  REFRESH_TOKEN_URL,
+  LOGIN_URL,
+  REGISTER_URL,
+  BASE_COURSE_URL,
+};
